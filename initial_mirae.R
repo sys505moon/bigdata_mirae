c69eb61e2d3acec22920bf7ebeafd308eef25a26
MoonJongHo <wildpig458@gmail.com>
2017-05-02 23:06:28 +0900

    1. 종목별 데이터 묶음 2. end_price를 통한 수익률 계산

--- initial_mirae.R
+++ initial_mirae.R
@@ -2,11 +2,13 @@
 # install.packages("xlsx")
 # install.packages("data.table")
 # install.packages("ggplot2")
+# install.packages("Hmisc")
 
 # Library
 library(xlsx)
 library(data.table)
 library(ggplot2)
+library(Hmisc)
 
 # directory setting
 getwd()
@@ -33,12 +35,89 @@ market_capital <- fread("market_capital.csv", sep = ',')
 dividend_tendency <- fread("dividend_tendency.csv", sep = ',')
 dividend_rate <- fread("dividend_rate.csv", sep = ',')
 
-
-
-
 data_table$time <- substr(data_table$time, 3, 7)data_table$time <- substr(data_table$time, 3, 7)
 colnames(data_table)[1] <- 'time'
 colnames(data_table)[2] <- 'apple'
+
+# colname change
+
+colnames(end_price)[2:length(end_price)] <- paste("end_price", colnames(end_price)[2:length(end_price)], sep = '_')
+colnames(high_price)[2:length(high_price)] <- paste("high_price", colnames(high_price)[2:length(high_price)], sep = '_')
+colnames(low_price)[2:length(low_price)] <- paste("low_price", colnames(low_price)[2:length(low_price)], sep = '_')
+colnames(volume)[2:length(volume)] <- paste("volume", colnames(volume)[2:length(volume)], sep = '_')
+colnames(PER)[2:length(PER)] <- paste("PER", colnames(PER)[2:length(PER)], sep = '_')
+colnames(PBR)[2:length(PBR)] <- paste("PBR", colnames(PBR)[2:length(PBR)], sep = '_')
+colnames(EPS)[2:length(EPS)] <- paste("EPS", colnames(EPS)[2:length(EPS)], sep = '_')
+colnames(sales)[2:length(sales)] <- paste("sales", colnames(sales)[2:length(sales)], sep = '_')
+colnames(business_profits)[2:length(business_profits)] <- paste("business_profits", colnames(business_profits)[2:length(business_profits)], sep = '_')
+colnames(net_profits)[2:length(net_profits)] <- paste("net_profits", colnames(net_profits)[2:length(net_profits)], sep = '_')
+colnames(market_capital)[2:length(market_capital)] <- paste("market_capital", colnames(market_capital)[2:length(market_capital)], sep = '_')
+colnames(dividend_tendency)[2:length(dividend_tendency)] <- paste("dividend_tendency", colnames(dividend_tendency)[2:length(dividend_tendency)], sep = '_')
+colnames(dividend_rate)[2:length(dividend_rate)] <- paste("dividend_rate", colnames(dividend_rate)[2:length(dividend_rate)], sep = '_')
+
+# Convert data.frame
+end_price <- data.frame(end_price)
+high_price <- data.frame(high_price)
+low_price <- data.frame(low_price)
+volume <- data.frame(volume)
+PER <- data.frame(PER)
+PBR <- data.frame(PBR)
+EPS <- data.frame(EPS)
+sales <- data.frame(sales)
+business_profits <- data.frame(business_profits)
+net_profits <- data.frame(net_profits)
+market_capital <- data.frame(market_capital)
+dividend_tendency <- data.frame(dividend_tendency)
+dividend_rate <- data.frame(dividend_rate)
+
+# group(item) data_integration  
+ # * 데이터들을 data.table 에서 data.frame 으로 변형해야 함
+item_names <- fread("item names.csv", sep =',', header = T)
+item_names <- t(item_names)
+item_names <- item_names[-1,]
+colnames(item_names) <- c("English", "Korean")
+item_names <- as.data.frame(item_names)
+item_strsplit <- data.frame(do.call('rbind', strsplit(as.character(item_names$English), split = ' ', fixed = TRUE)))
+
+variable_box = c()
+for (i in 1:length(item_strsplit$X1)){
+  variable_box <- c(variable_box, paste("x", item_strsplit[i,1], sep = '_'))
+}
+
+for (i in 1:length(item_strsplit$X1)){
+  assign(paste("x", item_strsplit[i,1], sep = '_'), cbind(end_price[c(1, 1+i)], high_price[i+1], low_price[i+1],
+                                                          volume[i+1], PER[i+1], PBR[i+1], EPS[i+1], sales[i+1],
+                                                          business_profits[i+1], net_profits[i+1], market_capital[i+1],
+                                                          dividend_tendency[i+1], dividend_rate[i+1]))
+}
+ # group(item) correlation search
+x_AAPL_cor <- rcorr(as.matrix(x_AAPL[2:14]), type = "pearson")$r
+
+# Income rate calculate
+
+item_bundle <- c(end_price, high_price)
+
+cal_income <- function(){
+  income = matrix(c(0), nrow = 204, ncol = 121)
+  for (i in 2:length(end_price$time)){
+    for (j in 2:length(end_price)){
+      income[i-1,j] <- (end_price[i,j]-end_price[i-1,j])/end_price[i-1,j]
+      }
+  }
+  return(income)
+}
+income_rate <- cal_income() # Calculation fot imcome_Ratio
+income_rate[,1] <- end_price$time
+income_rate[,1] <- paste(substr(income_rate[,1], 3, 4), substr(income_rate[,1],6,7), sep = '')
+
+
+
+
+
+ggplot(income_AAPL, aes(x=end_price.time, y=income, group=1))+geom_point()
+
+
+
 
 
 
@@ -48,7 +127,7 @@ colnames(data_table)[2] <- 'apple'
 
 # data_View
 
-View(data_table)
+
 View(data)
 ggplot(data = data_table, aes(x=time, y=apple))+geom_point()+theme(axis.text.x = element_text(angle = 45, size = 5))
 ggplot(data = data, aes(x=data[,1], y=data[,2], group = 1))+geom_point()+theme(axis.text.x = element_text(angle = 45, size = 5))
